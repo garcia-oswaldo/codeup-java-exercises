79bd77e6d81ed599c11a15ccf3d5bff9cae6b44a
oswaldo garcia <garcia.oswaldo1000@gmail.com>
2021-07-29 16:20:34 -0500

    first Java exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,179 @@
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello,World!");
+        System.out.println("Hello,World! today");
+        System.out.println("Hello,World! tomorrow");
+        int myFavoriteNumber = 32;
+        System.out.println(myFavoriteNumber);
+        String myString = "hello world";
+//        System.out.println(myString);
+//       String myString = "3.14159";
+//
+//        System.out.println(myString);
+//        long myNumber =;
+//        System.out.println(myNumber);
+//        illegal start of expression
+//        long myNumber = 123L;
+//        System.out.println(myNumber);
+        //show double for number. 3.14 did not run
+        // because it was decinal
+//        long myNumber = 123;
+//        System.out.println(myNumber);
+
+        // interger does not have decimals and 3.14 does
+        // and long can not process it.
+//        float myNumber = 3.14;
+//        System.out.println(myNumber);
+
+        float myNumber = 3.14f;
+        double myNumber2 =myNumber;
+        System.out.println(myNumber2);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        // increments after x so remains same
+//        System.out.println(x);
+//        //call to x shows 6 after incrementing
+
+        int x = 5;
+        System.out.println(++x);
+        // incremented by 1 and equals 6
+        System.out.println(x);
+        //prints out 6 as final result of increment.
+
+//        int class. = 5;
+//        //I get many errors
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) "three";
+        //inconvertable types
+
+        //13.
+
+
+        //14.
+
+
+
+
+
+
+        //JS way
+        // let nameOfVariable = 123 (put anything you want)
+
+      //Java Way
+       // int newNumber;
+       // newNumber=5;
+
+        //or int newNumber=5;
+
+//        byte dayOfMonth =31;
+//        short employees= 129;
+//        int carPrice= 50000;
+//        long diameterOfMilkyWay=78888888888L;
+        // n    eed to add capital L at end of number
+        // for it to be considered a long.
+
+//        //Decimal types
+//        //float 7 places decimal
+//        float gradeAverage = 93.35f;
+//        // for things like class grades
+//        double testDouble = 78888888888L;
+//        //very precise measuremewnts
+//        char yesNo = 'Y';
+//        boolean ILikeTacos = true;
+////      Strings
+        // strings are not primitive  types
+        // string always double quotes.
+//
+//        String name = "Ry Sutton";
+//        String criminalMindsQuote = "\"";
+
+//        "\"" // => "  shows quote
+//        "\\" // => \  shows slash
+//        "\n" // => the newline character
+//        "\t" // => the tab character
+
+//       Variables
+
+        // naming conventions camelCase for variables
+        // and PascalCase for class names.
+
+//        start with a letter, underscore, or dollar sign.
+//        consist of letters, dollar signs, underscores, and digits.
+//                be no more than 255 characters.
+
+//        Dont use these words
+//        abstract continue for new switch
+//        assert default goto package synchronized
+//        boolean do if private this break double
+//        implements protected throw byte else import
+//        public throws case enum instanceof return
+//        transient catch extends int short try char
+//        final interface static void class finally
+//        long strictfp volatile const float native
+//        super while
+
+//        CONSTANNTS
+//        final is the same as const
+//        final int SIDES_OF_A_DICE = 6;
+//        final String API_KEY = "b75b703d8195f6f433ca";
+//        final String GITHUB_API_BASE_URL =
+//                "https://api.github.com";
+
+//        CANT REASSIGN Values TO FINAL
+
+//        ARITHMATIC EXPRESSIONS
+//
+//        Binary
+//        int sum = 5+5;
+//
+//        int difference  = 6-5;
+//        int multiply = 5*6;
+//        double divide = 18/3f;
+//        int remainder = 4 % 2;
+//
+//        System. out.println(sum);
+//        System.out.println(difference)
+//        System.out.println(multiply)
+//        System.out.println(divide);
+//        System.out.println(remainder);
+//
+//
+//        + - * / %
+//
+//        unary operator
+//        ++ -- + -
+//
+//        int someNumber = 5;
+//        System.out.println(++someNumber);
+//        //returns 6
+//        int someNumber = 5;
+//        System.out.println(--someNumber);
+//        //returns 4
+//        System.out.println(someNumber++)
+//        //returns 5
+
+//        CASTING
+
+        //a. implicit is  taking from a less precise type to
+        // a more precise.
+
+//      int myInteger = 900;
+//      long morePrecise = myInteger;
+
+//        explicit- going from a less specific type to a more specific
+//                type.
+
+//        double pi = 3.14159;
+//        int almostPi = (int) pi;
+
+
+
+
     }
 
 }
