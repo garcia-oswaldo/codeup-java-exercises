0ee4ed5ec66c2a861f4c9797af0f2046444228c0
oswaldo garcia <garcia.oswaldo1000@gmail.com>
2021-08-03 15:54:53 -0500

    stared methods exercises

---
+++ src/MethodsExercises.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class MethodsExercises {
+
+    public static double add(double add){
+    return
+}
+
+    public static double subtract(double subtract){
+
+    }
+
+    public static double muliply(double multiply){
+
+    }
+
+    public static double divide(double divide){
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
