a775b9ef77f9701d08b0047509de17898d46cfb0
oswaldo garcia <garcia.oswaldo1000@gmail.com>
2021-08-03 10:12:06 -0500

    did run throught for the controlflowexercises

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -5,11 +5,11 @@ public class ControlFlowExercises {
         Scanner scanner = new Scanner(System.in);
 //        int i = 5;
 //        while (i <= 15) {
-//            System.out.println(i);
+//            System.out.println(i + " ");
 //            i++;
 //        }
 
-//        int i = 2;
+//        int i = 0;
 //        do {
 //            System.out.println(i);
 //            i = 2+i;
@@ -24,6 +24,11 @@ public class ControlFlowExercises {
 //        while (i >= -10);
 
 //
+//        long twoCounter =2L;
+//        do{
+//          System.out.println(twoCounter);
+//          twoCounter*=twoCounter;
+//        }while(twoCounter<1000000L);
 
 //        for (int i = 0; i <= 15; i++) {
 //            System.out.println(i);
@@ -40,10 +45,69 @@ public class ControlFlowExercises {
 //
 //        }
 
-        for (long i = 2; i < 1000000; i = (long) Math.pow(i,2)) {
-            System.out.println(i);
+//        for (long i = 2; i < 1000000; i = (long) Math.pow(i,2)) {
+//            System.out.println(i);
+//
+//        }
 
-        }
-    }
-}
+        //fizz buzz
+
+//        for (int i = 1; i <= 100; i++) {
+//            if (i % 5 == 0 && i % 3 == 0) {
+//                System.out.println(i + "fizzbuzz");
+//            } else if (i % 5 == 0) {
+//                System.out.println(i + "Buzz");
+//            } else if (i % 3 == 0) {
+//                System.out.println(i + "fizz");
+//            } else {
+//                System.out.println(i);
+//            }
+//        }
+
+        //Display table of powers
+//        boolean userContinues = true;
+//        do{
+//            System.out.println("What number would you like to go up to?");
+//            int userInt=scanner.nextInt();
+//            System.out.println("\nhere is your table\n");
+//            System.out.println("Number | squared| Cubed|");
+//            for (int i = 1; i <= userInt ; i++) {
+//                System.out.printf("%-6d", i);
+//                System.out.println(" | ");
+//                System.out.printf("%-7d", i*i);
+//                System.out.println(" | ");
+//                System.out.print(i*i*i);
+//                System.out.println();
+//
+//            }
+//            System.out.println("would you like to enter another number (Y/N)");
+//            String userResponse = scanner.next();
+//            if (userResponse.equalsIgnoreCase("y")){
+//                userContinues = false;
+//            }
+//        } while(userContinues);
+//
+//        boolean anotherGrade = true;
+//
+//        do{
+//            System.out.println("Please enter a numberical grade from 0-100");
+//            int userGrade = scanner.nextInt();
+//            if(userGrade >= 88){
+//                System.out.println("A");
+//            }else if (userGrade >= 80){
+//                System.out.println("B");
+//            }else if(userGrade >=67){
+//                System.out.println("C");
+//            }else if(userGrade>=60){
+//                System.out.println("D");
+//            }else {
+//                System.out.println("F");
+//            }
+//            System.out.println("do you want to enter another grade? Y/N?");
+//            String userResponse= scanner.next();
+//            if (!userResponse.equalsIgnoreCase("y")){
+//                            anotherGrade = false;}
+//        }while(anotherGrade);
+//    }
+//}
 
