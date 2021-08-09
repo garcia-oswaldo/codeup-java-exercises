2ff741c5c1a7eec1fb6165e1586b4313dd79ff2b
oswaldo garcia <garcia.oswaldo1000@gmail.com>
2021-08-09 10:52:38 -0500

    inheretence

---
+++ src/shapes/Rectangle.java
@@ -0,0 +1,15 @@
+package shapes;
+
+public class Rectangle {
+    protected int width;
+    protected int length;
+
+    public Rectangle(int width, int length) {
+        this.width = width;
+        this. length=length;
+    }
+    public int getArea(){
+        return length* width;
+    }
+    public int getPerimiter
+}
